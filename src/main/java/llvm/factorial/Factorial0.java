86fa4ad5e253be27b759ef7599433c20c99b0001
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2024-04-26 10:06:39 +0800

    +llvm.factorial

---
+++ src/main/java/llvm/factorial/Factorial0.java
@@ -0,0 +1,35 @@
+package llvm.factorial;
+
+/**
+ * @author: hfwei
+ * @date: 12/18/22
+ * @note:
+ */
+
+import org.bytedeco.javacpp.Pointer;
+import org.bytedeco.javacpp.PointerPointer;
+import org.bytedeco.llvm.LLVM.*;
+import static org.bytedeco.llvm.global.LLVM.*;
+
+public class Factorial0 {
+  public static void main(String[] args) {
+    LLVMInitializeCore(LLVMGetGlobalPassRegistry());
+    LLVMLinkInMCJIT();
+    LLVMInitializeNativeAsmPrinter();
+    LLVMInitializeNativeAsmParser();
+    LLVMInitializeNativeTarget();
+
+    LLVMModuleRef module = LLVMModuleCreateWithName("Factorial0");
+    LLVMBuilderRef builder = LLVMCreateBuilder();
+    LLVMTypeRef i32Type = LLVMInt32Type();
+
+    LLVMValueRef zero = LLVMConstInt(i32Type, 0, 0);
+    LLVMValueRef globalVar = LLVMAddGlobal(module, i32Type, "globalVar");
+    LLVMSetInitializer(globalVar, zero);
+
+    LLVMTypeRef returnType = i32Type;
+    PointerPointer<Pointer> argumentTypes = new PointerPointer<>(2)
+        .put(0, i32Type)
+        .put(1, i32Type);
+  }
+}
