6ba259a00aef01572d13d3cc895c776a786d159e
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2024-04-26 10:10:48 +0800

    Factorial0.java

--- src/main/java/llvm/factorial/Factorial0.java
+++ src/main/java/llvm/factorial/Factorial0.java
@@ -3,13 +3,28 @@ package llvm.factorial;
 /**
  * @author: hfwei
  * @date: 12/18/22
- * @note:
+ * @note: Unfinished!!! See Factorial1 and Factorial2 for full examples.
  */
+
+import static org.bytedeco.llvm.global.LLVM.LLVMAddGlobal;
+import static org.bytedeco.llvm.global.LLVM.LLVMConstInt;
+import static org.bytedeco.llvm.global.LLVM.LLVMCreateBuilder;
+import static org.bytedeco.llvm.global.LLVM.LLVMGetGlobalPassRegistry;
+import static org.bytedeco.llvm.global.LLVM.LLVMInitializeCore;
+import static org.bytedeco.llvm.global.LLVM.LLVMInitializeNativeAsmParser;
+import static org.bytedeco.llvm.global.LLVM.LLVMInitializeNativeAsmPrinter;
+import static org.bytedeco.llvm.global.LLVM.LLVMInitializeNativeTarget;
+import static org.bytedeco.llvm.global.LLVM.LLVMInt32Type;
+import static org.bytedeco.llvm.global.LLVM.LLVMLinkInMCJIT;
+import static org.bytedeco.llvm.global.LLVM.LLVMModuleCreateWithName;
+import static org.bytedeco.llvm.global.LLVM.LLVMSetInitializer;
 
 import org.bytedeco.javacpp.Pointer;
 import org.bytedeco.javacpp.PointerPointer;
-import org.bytedeco.llvm.LLVM.*;
-import static org.bytedeco.llvm.global.LLVM.*;
+import org.bytedeco.llvm.LLVM.LLVMBuilderRef;
+import org.bytedeco.llvm.LLVM.LLVMModuleRef;
+import org.bytedeco.llvm.LLVM.LLVMTypeRef;
+import org.bytedeco.llvm.LLVM.LLVMValueRef;
 
 public class Factorial0 {
   public static void main(String[] args) {
